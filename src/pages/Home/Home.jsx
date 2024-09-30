2784ec134ad9fd4f663512d89fac56b7044329c2
Aljoboyer <tanveeazmain@gmail.com>
2024-09-30 23:23:31 +0600

    text section is completed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Layout from '../../components/Shared/Layouts/Layouts'
 import Header from '../../components/Header/Header'
+import { TextSection } from '../../components/HomeComponents/TextSection'
 
 const Home = () => {
   return (
     <Layout>
         <Header/>
-        <div className='w-4/5	 lg:w-1/2 mx-auto py-24'>
-          <p className='leading-10 text-center font-light font-roboto text-[16px] md:text-[20px] lg:text-[26px] text-[#233]'>The CO2 footprint of the Internet's data centers alone may already be comparable to that of global air travel. The good news is that a growing number of major Internet corporations are becoming more environmentally conscientious, opting for more renewable energy sources for their data centers and operations.</p>
-        </div>
+       <TextSection textData="The CO2 footprint of the Internet's data centers alone may already be comparable to that of global air travel. The good news is that a growing number of major Internet corporations are becoming more environmentally conscientious, opting for more renewable energy sources for their data centers and operations."/>
+       <TextSection textData="These websites avoid bloated frameworks by using compressed graphics, efficient file formats, and lightweight fonts. Check out our information on developing sustainable websites and establishing a page weight budget if you want to learn more." buttonShow={true}/>
     </Layout>
   )
 }
